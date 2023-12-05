cdacab9295546e9418efa7d3973f45039ba0a2c0
randomguy169 <amaanhussain1210@gmail.com>
2023-12-05 08:03:45 +0530

    Add functions to sayName and twoNumbers

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,2 +1,15 @@
 //Functions in JavaScript
+function sayName() {
+  console.log("A");
+  console.log("m");
+  console.log("a");
+  console.log("a");
+  console.log("n");
+}
 
+// sayName();
+function twoNumbers(number1, number2) {
+  console.log(number1 + number2);
+
+}
+twoNumbers(10, 20);

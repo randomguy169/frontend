5e4242f32e8b750f7773cf17ef215a2e280fd887
Haidar <hhh.mcsd@gmail.com>
2024-01-17 14:04:08 +0530

    brr skibdiid baap baap

--- 07_projects/05_project/app.js
+++ 07_projects/05_project/app.js
@@ -1,7 +1,7 @@
 const colorName = document.querySelector("#color");
 const button = document.querySelector(".content");
 // console.log(colorName);
-hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 button.addEventListener("click", () => {
   hexColor = "#";
@@ -11,3 +11,12 @@ button.addEventListener("click", () => {
   document.body.style.backgroundColor = hexColor;
   colorName.innerHTML = hexColor;
 });
+
+button.onkeyup = () => {
+  hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[Math.floor(Math.random() * 16)];
+  }
+  document.body.style.backgroundColor = hexColor;
+  colorName.innerHTML = hexColor;
+};
